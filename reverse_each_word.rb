4548341e7d6dc6dd6a4766cb44c504269ec5ff20
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-02-09 16:34:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
-string = string.split
+original_string_array = string.split
 return_array = []
-
-
+original_string_array.each do |string|
+return_array << string.reverse 
+end
+return_array.joint(" ")
 end
